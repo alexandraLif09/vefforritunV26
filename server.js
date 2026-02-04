d5f574435cad265356c2936cc452b78252e1d844
Alexandra <alexadolph1701@gmail.com>
2026-02-04 10:26:53 +0000

    Lagaði/breytti server.js skjalinu

--- server.js
+++ server.js
@@ -1,37 +1,24 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+
+const moviesRouter = require('./src/routes/movies.routes');
 
 const app = express();
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const getMovies = () => {
-  const data = fs.readFileSync(path.join(__dirname, 'src/data/movies.json'));
-  return JSON.parse(data);
-};
+app.use('/', moviesRouter);
 
-app.get('/', (req, res) => {
-  const movies = getMovies();
-  res.render('index', { title: 'Forsíða', movies});
+app.use((req, res, next) => {
+  res.status(404).render('404', {title: 'Síða fannst ekki'});
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'Um mig' });
-});
-
-app.get('/movie/:id', (req, res) => {
-  const movies = getMovies();
-  const movie = movies.find(m => m.id === req.params.id);
-  
-  if (!movie) {
-    return res.status(404).render('404', {title: 'Síða fannst ekki'});
-  }
-
-  res.render('movie-details', {title: movie.title, movie});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Eitthvað fór úrskeiðis');
 });
 
 app.listen(PORT, () => {
