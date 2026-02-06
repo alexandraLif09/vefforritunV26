e425bc8a71f319e18d44b13bbd290a2e2606690f
Alexandra <alexadolph1701@gmail.com>
2026-02-06 10:17:07 +0000

    Gerði service.js skjalið

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,13 +1,13 @@
-const movieService = require('../lib/movieService');
+const movieService = require('../lib/Service');
 
 const index = (req, res) => {
-    const movies = movieService.getMovies();
+    const movies = Service.getMovies();
     res.render('index', {title: '', movies});
 };
 
 const details = (req, res) => {
     const {id} = req.params;
-    const movie = movieService.getMovieById(id);
+    const movie = Service.getMovieById(id);
 
     if (!movie) {
         return res.status(404).render('404', {title: 'Síða fannst ekki'});
